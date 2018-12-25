0771621b84b6082e2c5e51b3b659919eb2cdfa40
el3zahaby <el3zahaby@gmail.com>
2018-12-25 09:16:47 +0300

    hotel project v1.2.1 {Fix paths}

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
 session_start();
 
 
-define('base',dirname(__DIR__).'/htdocs');
+define('base','');
 define('base_url',getenv('HTTP_HOST').'/');
-define('path_config',base.'/app/config.php');
-include base.'/app/functions.php';
-include base.'/app/core.php';
+define('path_config',base.'app/config.php');
+include base.'app/functions.php';
+include base.'app/core.php';
 
 // route
-$folder = "/public";
+$folder = "public";
 $page = "index.php";
 
 
@@ -25,11 +25,12 @@ if(isset(explode("/",$_GET['data'])[0]) && explode("/",$_GET['data'])[0] != "") 
 }
 if (count(explode("/",$_GET['data']))>=0){
     $page = explode("/",$_GET['data'])[0].'.php';
-    $folder = "/public";
+    $folder = "public";
 
 }
+
 if(folder_exist(explode("/",$_GET['data'])[0]) ){
-    if (folder_exist(explode("/",$_GET['data'])[0]) != base){
+    if (explode("/",$_GET['data'])[0] != ""){
         $folder = explode("/",$_GET['data'])[0];
     }
     $page = "index.php";
@@ -45,9 +46,9 @@ $assets = "//".base_url.$folder;
 
 // includes
 include path_config;
-//var_dump(base."/".$folder.'/'.$page);
-if (file_exists(base."/".$folder.'/'.$page)){
-        require_once base."/".$folder.'/'.$page;
+// var_dump($folder.'/'.$page);
+if (file_exists($folder.'/'.$page)){
+    require_once $folder.'/'.$page;
 }else{
     http_response_code(404);
     include "404.php";
